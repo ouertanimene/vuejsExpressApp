465554cb9049e3789951bf86a5fa4d7738d4ae4d
ouertanimene <ouertanimene@gmail.com>
2021-10-12 23:37:47 +0200

    register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Register</h1>
+    <input name="email" type="email" placeholder="email" v-model="email" />
+    <br />
+    <input
+      name="password"
+      type="password"
+      placeholder="password"
+      v-model="password"
+    />
+    <button @click="register">Submit</button>
+  </div>
+</template>
+<script>
+import AuthentificationService from '../services/AuthentificationService'
+
+export default {
+  data () {
+    return {
+      email: ' ',
+      password: ' '
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthentificationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 
+app.use(bodyParser.json())
+
+
+
+
+app.use(cors())
 
 
 app.get('/', (req, res) => {
-  res.send({
-      message: 'hi '
-  })
+    res.send({
+        message: 'hi '
+    })
+})
+app.post('/eregister', (req, res) => {
+        res.send({
+           message: `user  ${req.body.email} well registred `
+       })
+   /** let newUser = {
+        id: Date.now(),
+        username: req.body.username,
+        email: req.body.email,
+        password: hashPassword,
+    };
+    users.push(newUser);*/
 })
 
 const port = process.env.PORT || 3000
